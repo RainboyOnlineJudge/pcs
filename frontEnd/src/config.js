b697b4cccc34c718d52e62f1e7acb512be9fa20d
Rainboy <597872644@qq.com>
2018-03-19 15:22:21 +0000

    [fix] 修改默认的server地址

--- frontEnd/src/config.js
+++ frontEnd/src/config.js
@@ -1,4 +1,4 @@
-var real_server = "http://server.rainboy.cc:3000/"
+var real_server = "/"
 var server = process.env.NODE_ENV==='production'?real_server:'http://localhost:3000/'
 module.exports = {
   server: server
